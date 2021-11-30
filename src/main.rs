3b0024d1cc036de349f2b2ecb731e58db04f2790
Nobuyoshi Aquino <nobu-aquino@pm.me>
2021-11-30 00:13:23 -0300

    Create TcpListener and print a message on each connection attempt.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        // A single stream represents an open connection 
+        // between the client and the server.
+        let _stream = stream.unwrap();
+
+        // If there aren't any errors, the program prints a message.
+        println!("Connection established!");
+
+        // When stream goes out of scope and is dropped at the end
+        // of the loop, the connection is closed as part of the drop
+        // implementation.
+    }
 }
