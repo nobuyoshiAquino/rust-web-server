46ba1ccd2c95fca932ab748123afff077a14303e
Nobuyoshi Aquino <nobu-aquino@pm.me>
2021-12-01 00:54:07 -0300

    Send HTTP 200 OK response for every client request.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,13 +8,28 @@ fn main() {
     for stream in listener.incoming() {
         // A single stream represents an open connection 
         // between the client and the server.
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        // If there aren't any errors, the program prints a message.
-        println!("Connection established!");
+        handle_connection(stream);
 
         // When stream goes out of scope and is dropped at the end
         // of the loop, the connection is closed as part of the drop
         // implementation.
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    // Read bytes from the TcpStream and put them in the buffer.
+    stream.read(&mut buffer).unwrap();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+
+    // `flush` will wait and prevent the program
+    // from continuing until all the bytes are written
+    // to the connection.
+    stream.flush().unwrap();
 }
