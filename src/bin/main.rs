ef02990b3352bf3ce93ef4abc09d67f5098a6a96
Nobuyoshi Aquino <nobu-aquino@pm.me>
2021-12-03 00:51:09 -0300

    Create thread pool to handle connections concurrently

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,16 +3,21 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+use web_server::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         // A single stream represents an open connection 
         // between the client and the server.
         let stream = stream.unwrap();
 
-        handle_connection(stream);
-
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+        
         // When stream goes out of scope and is dropped at the end
         // of the loop, the connection is closed as part of the drop
         // implementation.
